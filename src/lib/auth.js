c1bcf4e40a76e613af6683b7a3a223dc2d74f53f
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-04-30 18:55:48 +0600

    feat(db): implement MongoDB connection logic with environment variables

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db();
+const db = client.db("AutoGlaze");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
